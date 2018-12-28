63f8425c07cc61d21f46a4f48407f94278219cdf
zy <ljxyweb@163.com>
2018-12-28 15:50:17 +0800

    react组件模板生成脚手架

---
+++ README.md
@@ -0,0 +1,33 @@
+# react-cv-cli
+
+react-cv-cli 是一个 react 组件开发工具。有两个可供选择的模板，react-component-rollup-tpl 和 react-component-webpack-tpl。
+
+## Getting Started
+
+install:
+
+```
+npm install -g react-cv-cli
+# OR
+yarn global add react-cv-cli
+```
+
+Create a project:
+
+```
+react-cv-cli init my-component
+```
+
+## Templates
+
+### react-component-rollup-tpl
+
+react-component-rollup-tpl 用于编写 js 库，可对 js 和 css 进行处理，适用于 js 库。使用 webpack 生成开发环境，使用 rollup 进行打包，可打包成三个版本进行使用。
+
+### react-component-webpack-tpl
+
+react-component-webpack-tpl 适用于业务场景丰富的组件，支持处理 js、图片、css、字体等。使用 webpack 生成开发环境和编译打包。
+
+## License
+
+Licensed under the MIT License
---
+++ bin/index.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const program = require("commander"); //可以自动的解析命令和参数，用于处理用户输入的命令
+const download = require("download-git-repo"); //下载并提取 git 仓库，用于下载项目模板
+const handlebars = require("handlebars"); //模板引擎，将用户提交的信息动态填充到文件中。
+const inquirer = require("inquirer"); //通用的命令行用户界面集合，用于和用户进行交互
+const ora = require("ora"); //下载过程久的话，可以用于显示下载中的动画效果。
+const chalk = require("chalk"); //可以给终端的字体加上颜色。
+const symbols = require("log-symbols"); //可以在终端上显示出 √ 或 × 等的图标。
+
+program
+  .version("1.0.0", "-v, --version")
+  .usage("<command>") // 定义使用方法
+  .command("init <name>")
+  .description("Generate a new project")
+  .action(name => {
+    if (!fs.existsSync(name)) {
+      inquirer
+        .prompt([
+          {
+            name: "option",
+            type: "list",
+            message: "请选择模板编号:",
+            choices: [
+              {
+                name: "react-component-webpack-tpl",
+                value: "1"
+              },
+              {
+                name: "react-component-rollup-tpl",
+                value: "2"
+              }
+            ]
+          },
+          {
+            name: "description",
+            message: "请输入项目描述"
+          },
+          {
+            name: "author",
+            message: "请输入作者名称"
+          }
+        ])
+        .then(answers => {
+          if (answers.option !== "1" && answers.option !== "2") {
+            // 错误提示:输入的模板编号不正确。
+            console.log(symbols.error, chalk.red("输入的模板编号不正确"));
+            return;
+          }
+
+          const spinner = ora("正在下载模板...");
+          const tpl =
+            answers.option === "1"
+              ? "react-component-webpack-tpl"
+              : "react-component-rollup-tpl";
+          spinner.start();
+          download("ljxyweb/" + tpl, name, { clone: true }, err => {
+            if (err) {
+              spinner.fail();
+              console.log(symbols.error, chalk.red(err));
+            } else {
+              spinner.succeed();
+              const fileName = `${name}/package.json`;
+              const meta = {
+                name,
+                description: answers.description,
+                author: answers.author
+              };
+              if (fs.existsSync(fileName)) {
+                const content = fs.readFileSync(fileName).toString();
+                const result = handlebars.compile(content)(meta);
+                fs.writeFileSync(fileName, result);
+              }
+              console.log(symbols.success, chalk.green("项目初始化完成"));
+            }
+          });
+        });
+    } else {
+      // 错误提示项目已存在，避免覆盖原有项目
+      console.log(symbols.error, chalk.red("项目已存在"));
+    }
+  });
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
